158a0e4a3bef541b32510f24b78c8d9ebc20719f
josephmansilla <jomansilla@frba.utn.edu.ar>
2025-04-28 23:23:47 -0300

    guia2 cambios finales

--- guia/guia2.hs
+++ guia/guia2.hs
@@ -1,5 +1,9 @@
 -- APLICACION PARCIAL
 -- Ejercicio 1
+
+siguiente' :: Integer -> Integer
+siguiente' numero = numero + 1
+
 siguiente :: Integer -> Integer
 siguiente = (+1)
 
@@ -8,40 +12,40 @@ mitad :: Double -> Double
 mitad = (/2)
 
 -- Ejercicio 3
-inversa :: Fractional a => a -> a
-inversa x = 1 / x
-
-inversaParcial :: Double -> Double
-inversaParcial = inversa
+inversa':: Double -> Double 
+inversa' = (1/) 
 
 -- Ejercicio 4
 triple :: Integer -> Integer
 triple = (*3)
 
 -- Ejercicio 5
-pos :: (Ord a, Num a) => a -> Bool
+pos :: Integer -> Bool
 pos = (>0)
 esNumeroPositivo :: Integer -> Bool
 esNumeroPositivo = pos
 
 -- COMPOSICION
 -- Ejercicio 6
-esMultiploDe :: Integral b => b -> b -> Bool
-esMultiploDe n = (==0) . (n `mod`)
+esMultiploDe :: Integer -> Integer -> Bool
+esMultiploDe dividendo = (==0). (dividendo `mod`) 
 
 -- Ejercicio 7
 
 --esBisiesto n = (not (100 `esMultiploDe`)).(||).(4 `esMultiploDe` ).(&&).(400 `esMultiploDe`)
 
--- Ejercicio 8
+-- Ejercicio 8 
 inversaRaizCuadrada :: Double -> Double
-inversaRaizCuadrada = inversaParcial . sqrt 
+inversaRaizCuadrada = inversa' . sqrt 
 
 -- Ejercicio 9
 incrementMCuadradoN:: Double -> Double -> Double
 incrementMCuadradoN m = (+m).(^2) 
 
 -- Ejercicio 10
+elevar' :: (Integral b, Num a) => a -> b -> a
+elevar' = (^)
+
 elevar :: (Integral b, Num a) => a -> b -> a
 elevar = (^) 
 
